89437962cfcfedb768f4e377bda542da657d789f
agu <iba.agustin@gmail.com>
2023-03-30 22:37:25 -0300

    set up passport local strategy. Name and password.

--- app.js
+++ app.js
@@ -25,12 +25,43 @@ const app = express();
 app.set("views", __dirname);
 app.set("view engine", "ejs");
 
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: "wrong username" });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: "wrong password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => res.render("index"));
-app.get;
+app.get("/sign-up", (req, res) => res.render("signUpForm"));
+
+app.post("/signUpForm", (req, res, next) => {
+  try {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password,
+    });
+
+    const result = user.save();
+    res.redirect("/");
+  } catch (err) {
+    return next(err);
+  }
+});
 
 app.listen(3000, () => console.log("app is listening on port 3000!"));
